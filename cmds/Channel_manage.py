09dc73ad48a8f4059f524d5f4ea06168422f9c3b
HYBBWuXiDiXi <2558985693@qq.com>
2022-11-25 22:56:58 +0800

    nothing changed:)

--- cmds/Channel_manage.py
+++ cmds/Channel_manage.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 from Basic_bot.Core.init_cog import InitCog
 from Basic_bot.Core import loadjson
 
-jdata = loadjson.load()
+jdata = loadjson.load_mainconfig()
 
 
 class ChannelManage(InitCog):
@@ -16,11 +16,6 @@ class ChannelManage(InitCog):
     async def on_member_remove(self, member):
         levchannel = self.client.get_channel(int(jdata['welchannel-id']))
         await levchannel.send(f'{member} 退出了频道')
-
-    @commands.Cog.listener()
-    async def on_message(self, msg):
-        if msg.content == 'hi' and msg.author != self.client.author:
-            await msg.channel.send('hi')
 
 
 async def setup(client):
