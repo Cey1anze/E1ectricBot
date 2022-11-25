bd085f47652481b4be245a5f1639ccccc753bc37
HYBBWuXiDiXi <2558985693@qq.com>
2022-11-25 22:58:46 +0800

    add new load module

--- Core/loadjson.py
+++ Core/loadjson.py
@@ -1,7 +1,19 @@
 import json
 
 
-def load():
-    with open('./config.json', 'r', encoding='UTF-8') as jf:
+def load_mainconfig():
+    with open('./configs/config.json', 'r', encoding='UTF-8') as jf:
         jdata = json.load(jf)
         return jdata
+
+
+def load_blockwordconfig():
+    with open('./configs/blockwords.json', 'r', encoding='UTF-8') as jf:
+        keydata = json.load(jf)
+        return keydata
+
+
+def load_replyconfig():
+    with open('./configs/replyDict.json', 'r', encoding='UTF-8') as jf:
+        replydata = json.load(jf)
+        return replydata
