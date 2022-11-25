efa05b9214d6a52afb46da80a701bfde74bb331e
HYBBWuXiDiXi <2558985693@qq.com>
2022-11-25 11:29:11 +0800

    Added Message module

---
+++ cmds/Message.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from Basic_bot.Core.init_cog import InitCog
+
+
+class Message(InitCog):
+
+    @commands.command(name='clean')
+    async def clean(self, ctx, num: int):
+        await ctx.channel.purge(limit=num + 1)
+        await ctx.send(f'{num} messages deleted')
+
+    @commands.command(name='resay')
+    async def resay(self, ctx, *, msg):
+        await ctx.message.delete()
+        await ctx.send(msg)
+
+
+async def setup(client):
+    await client.add_cog(Message(client))
