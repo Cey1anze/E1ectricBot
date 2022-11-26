c305fd45113afdb4dffbb685bba9c68225938977
HYBBWuXiDiXi <2558985693@qq.com>
2022-11-26 23:23:14 +0800

    add function that give a role when member join

--- cmds/Channel.py
+++ cmds/Channel.py
@@ -1,3 +1,4 @@
+from discord.utils import get
 from discord.ext import commands
 from Basic_bot.Core.init_cog import InitCog
 from Basic_bot.Core import loadjson
@@ -7,10 +8,15 @@ jdata = loadjson.load_mainconfig()
 
 class ChannelManage(InitCog):
 
+    # Response when a new member join
     @commands.Cog.listener()
     async def on_member_join(self, member):
         welchannel = self.client.get_channel(int(jdata['welchannel-id']))
+        # Optional : give a role when new members join,id = role_id,Separate roles from online status
+        role = get(member.guild.roles, id=1046023381612511302)
         await welchannel.send(f'欢迎 {member} 进入频道')
+        # give a role when new member joined,id = role_id
+        await member.add_roles(role)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
