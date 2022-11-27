139615853d62c12e06388de99d7fbc97da3b4e2a
HYBBWuXiDiXi <2558985693@qq.com>
2022-11-27 20:17:28 +0800

    response by embed

--- Basic_main.py
+++ Basic_main.py
@@ -18,20 +18,48 @@ async def setup_hook():
 
 @client.command(name='load', help='load cogs')
 async def load(ctx, extension):
-    await client.load_extension(f'cmds.{extension}')
-    await ctx.send(f'{extension} loaded')
+    try:
+        await client.load_extension(f"cmds.{extension}")
+        load_embed = discord.Embed(color=discord.Color.from_rgb(130, 156, 242),
+                                   description=f"**{extension}** 已成功载入")
+        await ctx.send(embed=load_embed)
+    except commands.ExtensionNotFound:
+        load_embed = discord.Embed(color=discord.Color.from_rgb(130, 156, 242),
+                                   description=f"**{extension}** 没有找到")
+        await ctx.send(embed=load_embed)
+    except commands.ExtensionAlreadyLoaded:
+        load_embed = discord.Embed(color=discord.Color.from_rgb(130, 156, 242),
+                                   description=f"**{extension}** 已被加载，无需再加载")
+        await ctx.send(embed=load_embed)
+        return
 
 
 @client.command(name='unload', help='unload cogs')
 async def unload(ctx, extension):
-    await client.unload_extension(f'cmds.{extension}')
-    await ctx.send(f'{extension} unloaded')
+    try:
+        await client.unload_extension(f"cmds.{extension}")
+        unload_embed = discord.Embed(color=discord.Color.from_rgb(130, 156, 242),
+                                     description=f"**{extension}** 已成功卸载")
+        await ctx.send(embed=unload_embed)
+    except commands.ExtensionNotLoaded:
+        unload_embed = discord.Embed(color=discord.Color.from_rgb(130, 156, 242),
+                                     description=f"**{extension}** 没有找到 或 已被卸载，无需再次卸载")
+        await ctx.send(embed=unload_embed)
+        return
 
 
 @client.command(name='reload', help='reload cogs')
 async def reload(ctx, extension):
-    await client.reload_extension(f'cmds.{extension}')
-    await ctx.send(f'{extension} re-loaded')
+    try:
+        await client.reload_extension(f"cmds.{extension}")
+        reload_embed = discord.Embed(color=discord.Color.from_rgb(130, 156, 242),
+                                     description=f"**{extension}** 已重新载入")
+        await ctx.send(embed=reload_embed)
+    except commands.ExtensionNotLoaded:
+        reload_embed = discord.Embed(color=discord.Color.from_rgb(130, 156, 242),
+                                     description=f"**{extension}** 没有找到 或 已被卸载，请先载入")
+        await ctx.send(embed=reload_embed)
+        return
 
 
 @client.event
