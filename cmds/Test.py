dbcc16d20c6d189048b42ebe5cb418d8ea768522
HYBBWuXiDiXi <2558985693@qq.com>
2022-11-27 20:15:49 +0800

    change response to embed

--- cmds/Test.py
+++ cmds/Test.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+
 from Basic_bot.Core.init_cog import InitCog
 
 
@@ -7,7 +8,9 @@ class Test(InitCog):
 
     @commands.command(name='ping', help='Test bot delay')
     async def ping(self, ctx):
-        await ctx.send(f'机器人延迟：{round(self.client.latency * 1000)}ms')
+        await ctx.channel.purge(limit=1)
+        await ctx.send(embed=discord.Embed(description=f'🕒延迟：{round(self.client.latency * 1000)} ms',
+                                           colour=discord.Color.from_rgb(130, 156, 242)))
 
 
 async def setup(client):
