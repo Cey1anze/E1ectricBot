e31fabf83740001ff62da9dcd038d7de8c615559
HYBBWuXiDiXi <2558985693@qq.com>
2022-11-29 15:58:29 +0800

    nothing changed

--- cmds/Member.py
+++ cmds/Member.py
@@ -8,10 +8,10 @@ from discord.utils import get
 class Member(InitCog):
 
     # give someone a role by commands,example: ?add-role @user [true/false] [old-role's name] [new-role's name]
-    # this example means user joined test1 and removed from test : ?add-role @user true test1 test
+    # this example means user joined test and removed from test1 : ?add-role @user true test1 test
     # old-role can be multiple
     # true:remove from old-role and join new-role,false:join new-role directly
-    @commands.command(name='add-role', help='give member a role')
+    @commands.command(name='add-role', help='给予身份组')
     @commands.is_owner()
     async def addrole(self, ctx, arg: discord.Member, arg1: bool, arg2: str, *, arg3: str):
         oldrole = get(ctx.guild.roles, name=arg2)
@@ -28,7 +28,7 @@ class Member(InitCog):
                                                colour=discord.Color.from_rgb(130, 156, 242)))
 
     # kick member by command,example: ?kick @user
-    @commands.command(name='kick', help='kick member from guild')
+    @commands.command(name='kick', help='踢除用户')
     @commands.is_owner()
     async def kickmember(self, ctx, member: discord.Member, *, reason=None):
         await member.kick(reason=reason)
@@ -37,7 +37,7 @@ class Member(InitCog):
                                            colour=discord.Color.from_rgb(130, 156, 242)))
 
     # ban member by command,example: ?ban @user
-    @commands.command(name='ban', help='ban member from guild')
+    @commands.command(name='ban', help='封禁用户')
     @commands.is_owner()
     async def ban(self, ctx, member: discord.Member, *, reason=None):
         await member.ban(reason=reason)
@@ -46,7 +46,7 @@ class Member(InitCog):
                                            colour=discord.Color.from_rgb(130, 156, 242)))
 
     # unban member by command,example: ?unban @user
-    @commands.command(name='unban', help='ban member from guild')
+    @commands.command(name='unban', help='解封用户')
     @commands.guild_only()
     @commands.is_owner()
     async def unban(self, ctx, userId):
