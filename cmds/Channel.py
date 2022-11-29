43725a855df840036bbd4f7f80e9c60fae2141ce
HYBBWuXiDiXi <2558985693@qq.com>
2022-11-29 15:58:03 +0800

    nothing changed

--- cmds/Channel.py
+++ cmds/Channel.py
@@ -4,6 +4,8 @@ from Basic_bot.Core.init_cog import InitCog
 from Basic_bot.Core import loadjson
 
 jdata = loadjson.load_mainconfig()
+channel = loadjson.load_channelconfig()
+roles = loadjson.load_roleconfig()
 
 
 class ChannelManage(InitCog):
@@ -11,16 +13,16 @@ class ChannelManage(InitCog):
     # Response when a new member join
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        welchannel = self.client.get_channel(int(jdata['welchannel-id']))
+        welchannel = self.client.get_channel(int(channel['welchannel-id']))
         # Optional : give a role when new members join,id = role_id,Separate roles from online status
-        role = get(member.guild.roles, id=1046023381612511302)
+        role = get(member.guild.roles, id=int(roles['test']))
         await welchannel.send(f'欢迎 {member} 进入频道')
         # give a role when new member joined,id = role_id
         await member.add_roles(role)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        levchannel = self.client.get_channel(int(jdata['welchannel-id']))
+        levchannel = self.client.get_channel(int(channel['welchannel-id']))
         await levchannel.send(f'{member} 退出了频道')
 
 
