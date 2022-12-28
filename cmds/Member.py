63ab3d45ed2dfb782ec4a0279118608567fddbf6
HYBBWuXiDiXi <2558985693@qq.com>
2022-12-28 17:22:44 +0800

    Add Mute Function

--- cmds/Member.py
+++ cmds/Member.py
@@ -1,6 +1,9 @@
+import datetime
 import discord
 import json
+from discord.app_commands import Choice
 from discord.ext import commands
+from discord import app_commands
 from Basic_bot.Core.init_cog import InitCog
 from discord.utils import get
 
@@ -44,6 +47,85 @@ class Member(InitCog):
         await ctx.channel.purge(limit=1)
         await ctx.send(embed=discord.Embed(description=f'{member} 已被封禁，原因:{reason}',
                                            colour=discord.Color.from_rgb(130, 156, 242)))
+
+    @app_commands.command(name='mute', description='静音用户')
+    @app_commands.describe(reason='静音的原因')
+    async def mute(self, interaction: discord.Interaction, member: discord.Member, *, reason: str = None):
+        if interaction.user.guild_permissions.mute_members:
+            try:
+                voice_state = member.voice
+                if voice_state:
+                    await interaction.response.defer()
+                    await member.edit(mute=True, reason=reason)
+                    await interaction.followup.send(embed=discord.Embed(description=f'{member} 已被静音 , 原因 : {reason}',
+                                                                        colour=discord.Color.from_rgb(130, 156, 242)))
+                else:
+                    await interaction.response.defer()
+                    await interaction.followup.send(embed=discord.Embed(description=f'**{member}** 不在任何语音频道中',
+                                                                        colour=discord.Color.from_rgb(130, 156, 242)))
+            except Exception as e:
+                print(e)
+        else:
+            await interaction.response.defer()
+            await interaction.followup.send(embed=discord.Embed(description=f'{interaction.user.mention} 没有执行这项操作的权限',
+                                                                colour=discord.Color.from_rgb(130, 156, 242)))
+
+    @app_commands.command(name='timeout', description='禁言用户')
+    @app_commands.rename(option='禁言时间')
+    @app_commands.describe(reason='禁言的原因')
+    @app_commands.choices(option=[
+        Choice(name='60秒', value=0),
+        Choice(name='5分钟', value=1),
+        Choice(name='10分钟', value=2),
+        Choice(name='1小时', value=3),
+        Choice(name='1天', value=4),
+        Choice(name='1周', value=5)
+    ])
+    async def timeout(self, interaction: discord.Interaction, member: discord.Member, option: int, *, reason: str = None):
+        if interaction.user.guild_permissions.moderate_members:
+            try:
+                if option == 0:
+                    await interaction.response.defer()
+                    await member.timeout(datetime.timedelta(minutes=1.0), reason=reason)
+                    await interaction.followup.send(
+                        embed=discord.Embed(description=f'{member} 已被禁言 **1 分钟**, 原因 : {reason}',
+                                            colour=discord.Color.from_rgb(130, 156, 242)))
+                elif option == 1:
+                    await interaction.response.defer()
+                    await member.timeout(datetime.timedelta(minutes=5.0), reason=reason)
+                    await interaction.followup.send(
+                        embed=discord.Embed(description=f'{member} 已被禁言 **5 分钟**, 原因 : {reason}',
+                                            colour=discord.Color.from_rgb(130, 156, 242)))
+                elif option == 2:
+                    await interaction.response.defer()
+                    await member.timeout(datetime.timedelta(minutes=10.0), reason=reason)
+                    await interaction.followup.send(
+                        embed=discord.Embed(description=f'{member} 已被禁言 **10 分钟**, 原因 : {reason}',
+                                            colour=discord.Color.from_rgb(130, 156, 242)))
+                elif option == 3:
+                    await interaction.response.defer()
+                    await member.timeout(datetime.timedelta(hours=1.0), reason=reason)
+                    await interaction.followup.send(
+                        embed=discord.Embed(description=f'{member} 已被禁言 **1 小时**, 原因 : {reason}',
+                                            colour=discord.Color.from_rgb(130, 156, 242)))
+                elif option == 4:
+                    await interaction.response.defer()
+                    await member.timeout(datetime.timedelta(weeks=1.0), reason=reason)
+                    await interaction.followup.send(
+                        embed=discord.Embed(description=f'{member} 已被禁言 **1 天**, 原因 : {reason}',
+                                            colour=discord.Color.from_rgb(130, 156, 242)))
+                elif option == 5:
+                    await interaction.response.defer()
+                    await member.timeout(datetime.timedelta(minutes=1.0), reason=reason)
+                    await interaction.followup.send(
+                        embed=discord.Embed(description=f'{member} 已被禁言 **1 周**, 原因 : {reason}',
+                                            colour=discord.Color.from_rgb(130, 156, 242)))
+            except Exception as e:
+                print(e)
+        else:
+            await interaction.response.defer()
+            await interaction.followup.send(embed=discord.Embed(description=f'{interaction.user.mention} 没有执行这项操作的权限',
+                                                                colour=discord.Color.from_rgb(130, 156, 242)))
 
     # unban member by command,example: ?unban @user
     @commands.command(name='unban', help='解封用户')
